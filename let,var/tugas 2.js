1682c907a6658fb80df4ead04f8807a222b09ac1
widyaelza <widyaelza8@gmail.com>
2024-06-14 15:14:02 +0700

    Create tugas 2.js

---
+++ let,var/tugas 2.js
@@ -0,0 +1,15 @@
+var w="nama";
+    w+=15
+    console.log(w)
+var f="23";
+    f+=45
+    console.log(f)
+var j="1";
+    j+="3";
+    console.log(j)
+var s=4
+    s+="5"
+    console.log(s)
+var c="15"
+    c+=56
+    console.log(c)
